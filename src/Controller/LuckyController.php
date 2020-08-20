1d4b4dbe05ff9b03d63a27442bb58d95d43759e3
Anastasy Chizhik <katenok-nastja@mail.ru>
2020-08-20 19:31:12 +0300

    уывп

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,7 +2,7 @@
 
 
 namespace App\Controller;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
