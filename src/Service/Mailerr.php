f7de0270bb76a6d4d0415084089ebb709c76895c
Anastasy Chizhik <katenok-nastja@mail.ru>
2020-08-29 13:13:44 +0300

    йцкйц

--- src/Service/Mailerr.php
+++ src/Service/Mailerr.php
@@ -13,78 +13,31 @@ use PHPMailer\PHPMailer\SMTP;
 
 class Mailerr
 {
+	  private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer=$mailer;
+    }
 
     public function sendConfirmationMessage()
     {
+        $email = (new Email())
+            ->from('katenok-nastja@mail.ru')
+            ->to('katenok-nastja@mail.ru')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('45345345Time for Symfony Mailer!')
+            ->text('234234234Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+           echo $e->getDebug();
+        }
 
-
-    
-
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
-
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-
-//Enable SMTP debugging
-// SMTP::DEBUG_OFF = off (for production use)
-// SMTP::DEBUG_CLIENT = client messages
-// SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-//Set the hostname of the mail server
-$mail->Host = 'smtp.mail.ru';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-
-//Set the encryption mechanism to use - STARTTLS or SMTPS
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = 'katenok-nastja@mail.ru';
-
-//Password to use for SMTP authentication
-$mail->Password = '221217Julia';
-
-//Set who the message is to be sent from
-$mail->setFrom('katenok-nastja@mail.ru', 'First Last');
-
-//Set an alternative reply-to address
-$mail->addReplyTo('katenok-nastja@mail.ru', 'First Last');
-
-//Set who the message is to be sent to
-$mail->addAddress('katenok-nastja@mail.ru', 'John Doe');
-
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-
-//Replace the plain text body with one created manually
-$mail->Body = 'This is a plain-text message body';
-
-
-//send the message, check for errors
-if (!$mail->send()) {
-    echo 'Mailer Error: '. $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
-}
-
-
-    
-	}}
+        // ...
+    }
+	}
